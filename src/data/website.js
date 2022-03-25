e4cce660abe312513232bb053d49550b5457b5ff
Virtus-web <nicolas.pedenau@hotmail.fr>
2022-03-25 16:44:29 +0100

    description sites

--- src/data/website.js
+++ src/data/website.js
@@ -5,8 +5,8 @@ export const websiteData = [
         title: "Site de mariage",
         description: "Description",
         site: "https://marriagecarineetpierre.netlify.app",
-        info1: "Utilisation de React App",
-        info2: "Intégration d'une API via Express et Mongo DB",
+        info1: "Ce site est uen application React en ligne et toujours en cours de constrction",
+        info2: "Il a été développé en parallèle d'une API construite avec Express et Mongo DB, et intègre un formulaire de validation. La prochaine étape sera d'y intégrer un forum de messagerie instantanée",
         info3: "Déploiement du site avec Heroku pour la partie backend et avec Netlify pour la partie frontend. Le code du site est entièrement hébergé sur GitHub"
     },
     {
@@ -15,9 +15,9 @@ export const websiteData = [
         title: "Site de suivi sportif",
         description: "Description",
         site: "https://Virtus-web.github.io/suivi-sportif",
-        info1: "Utilisation de React App",
-        info2: "Intégration d'une API via Express et Mongo DB",
-        info3: "Déploiement du site avec Heroku pour la partie backend et avec Netlify pour la partie frontend. Le code du site est entièrement hébergé sur GitHub"
+        info1: "Application React 100% desktop",
+        info2: "Ce site a été créé pour suivre les performances sportives des utilisateurs. Il intègre des graphiques développer via Recharts",
+        info3: "Ce projet a été réalisé dans le cadre de ma formation diplômante avec Open Classrooms. Le code du site est entièrement hébergé sur GitHub et l'application a été adaptée pour être déployée via GitHub Pages"
     },
     {
         id: 3,
@@ -25,9 +25,9 @@ export const websiteData = [
         title: "Site de location de vacances",
         description: "Description",
         site: "https://virtus-web.github.io/location-vacances",
-        info1: "Utilisation de React App",
-        info2: "Intégration d'une API via Express et Mongo DB",
-        info3: "Déploiement du site avec Heroku pour la partie backend et avec Netlify pour la partie frontend. Le code du site est entièrement hébergé sur GitHub"
+        info1: "Application React responsive",
+        info2: "Ce site a été développé pour répertorier des locations de vacances. Les utilisateurs peuvent cliquer sur chaque fiche afin de connaître la description du bien immobilier et avoir accès à plusieurs photos via un caroussel",
+        info3: "Ce projet a été réalisé dans le cadre de ma formation diplômante avec Open Classrooms. Le code du site est entièrement hébergé sur GitHub et l'application a été adaptée pour être déployée via GitHub Pages"
     },
     {
         id: 4,
@@ -35,9 +35,9 @@ export const websiteData = [
         title: "Site de recrutement",
         description: "Description",
         site: "https://virtus-web.github.io/agence-recrutement",
-        info1: "Utilisation de React App",
-        info2: "Intégration d'une API via Express et Mongo DB",
-        info3: "Déploiement du site avec Heroku pour la partie backend et avec Netlify pour la partie frontend. Le code du site est entièrement hébergé sur GitHub"
+        info1: "Application React 100% desktop",
+        info2: "Cette plateforme propose l'accès à un répertoire de profils de développeurs professionnels. Il ont la possibilité de répondre à un questionnaire afin de trouver le profil adéquat en fonction de leur besoins.",
+        info3: "Ce projet a été réalisé dans le cadre de ma formation diplômante avec Open Classrooms. Le code du site est entièrement hébergé sur GitHub et l'application a été adaptée pour être déployée via GitHub Pages"
     },
     {
         id: 5,
@@ -45,8 +45,8 @@ export const websiteData = [
         title: "Site de e-commerce",
         description: "Description",
         site: "https://virtus-web.github.io/e-shopping",
-        info1: "Utilisation de React App",
-        info2: "Intégration d'une API via Express et Mongo DB",
-        info3: "Déploiement du site avec Heroku pour la partie backend et avec Netlify pour la partie frontend. Le code du site est entièrement hébergé sur GitHub"
+        info1: "Application React 100% desktop",
+        info2: "Cette maquette de site de vente par correspondance propose aux utilisateurs une liste de produits avec la possibilité de les placer dans un panier d'achat",
+        info3: "Ce projet a été réalisé dans le cadre de ma formation diplômante avec Open Classrooms. Le code du site est entièrement hébergé sur GitHub et l'application a été adaptée pour être déployée via GitHub Pages"
     }
 ]
